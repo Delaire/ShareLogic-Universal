55d8bd4de2e36cf017009220a7b9a4c143173f69
Damien Delaire <delairedd@gmail.com>
2014-11-12 11:28:42 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+ShareLogic class for Universal Apps (C#)
+=========================
+
+This is a tutorial on how to use a class called ShareLogic which allows you to share information on your app, and more.
+
+Today, this class is working the following way:
+
+//invite your friends to use the app
+- ShareLogic.InviteFriends();
+
+//Send feedback back to the developer
+- ShareLogic.SendFeedback();
+
+//share app on twitter
+- ShareLogic.ShareViaTwitter();
+
+//show other apps developed by the developer 
+- ShareLogic.ShowMoreApps();
+
+//review the app
+- ShareLogic.ShowReviewTask();
+			
+
+This code is to be used as is, i cannot garanty that this code will work everywhere, however this is the code that i use in my personal apps: http://www.windowsphone.com/en-US/store/publishers?publisherId=Delaire%2BDamien
+
+I will try to add more tutorial on how i use different components in Windows Phone, Windows Store and Universal Windows Apps
+Damien
+
+
+License
+
+This project is licensed under the MIT license.
---
+++ ShareLogic-Demo/ShareLogic-Demo/ShareLogic-Demo.Shared/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using ShareLogic_Lib;
+
+namespace ShareLogic_Demo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ///Examples of how you can use the lib:
+            
+            //invite your friends to use the app
+            ShareLogic.InviteFriends();
+
+            //Send feedback back to the developer
+            ShareLogic.SendFeedback();
+
+            //share app on twitter
+            ShareLogic.ShareViaTwitter();
+
+            //show other apps developed by the developer 
+            ShareLogic.ShowMoreApps();
+
+            //review the app
+            ShareLogic.ShowReviewTask();
+
+           
+        }
+    }
+}
